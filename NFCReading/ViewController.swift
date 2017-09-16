dfc3dbf7a57c976cf8c2eb23f3c288a46db03386
Nelida Velazquez <nelida.velazquez@detroitlabs.com>
2017-09-16 17:25:09 -0400

    Creates main screen with button and label

--- NFCReading/ViewController.swift
+++ NFCReading/ViewController.swift
@@ -9,17 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var contentLabel: UILabel!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func scan(_ sender: UIButton) {
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
