7273723dc53df801450f29b1667f3b8e01afb48b
Nelida Velazquez <nelida.velazquez@detroitlabs.com>
2017-09-16 18:46:35 -0400

    Implements NDC reading

--- NFCReading/ViewController.swift
+++ NFCReading/ViewController.swift
@@ -7,12 +7,35 @@
 //
 
 import UIKit
+import CoreNFC
 
 class ViewController: UIViewController {
+    
+    var nfcSession: NFCNDEFReaderSession?
     
     @IBOutlet var contentLabel: UILabel!
 
     @IBAction func scan(_ sender: UIButton) {
+        nfcSession = NFCNDEFReaderSession.init(delegate: self, queue: nil, invalidateAfterFirstRead: true)
+        nfcSession?.begin()
+    }
+}
+
+extension ViewController: NFCNDEFReaderSessionDelegate {
+
+    func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
+        print("The session was invalidated: \(error.localizedDescription)")
+    }
+    
+    func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
+        var result = ""
+        for payload in messages[0].records {
+            result += String.init(data: payload.payload.advanced(by: 3), encoding: .utf8)!
+        }
+        
+        DispatchQueue.main.async {
+            self.contentLabel.text = result
+        }
     }
 }
 
